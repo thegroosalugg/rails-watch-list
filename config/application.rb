1d4e496d1aa9ec8b55d74b2c8328da06d2836f80
Victor Loginov <tkoe88@zoho.com>
2023-08-17 11:25:13 +0200

    base set up

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "rails/test_unit/railtie"
+import "bootstrap"
+import "@popperjs/core"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
